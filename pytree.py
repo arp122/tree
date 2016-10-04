751b2707c556e085a24319ec72e4c5c0e0c4fdda
arp122 <arpit.sabherwal@gmail.com>
2016-10-04 05:40:22 +0000

    First commit

--- pytree.py
+++ pytree.py
@@ -1,11 +1,46 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
+
+indent = "?~T~B   "
+indent_done = "    "
+child_branch = "?~T~\?~T~@?~T~@ "
+child_branch_done = "?~T~T?~T~@?~T~@ "
+file_count = 0
+dir_count = 0
 
 
-# YOUR CODE GOES here
+def tree_generate(path, symbol):
+    global dir_count
+    global file_count
+    all_children = sorted([c for c in os.listdir(path) if not c.startswith('.')])
+    end_file = 0
+    num_children = 0
+    for children in all_children:
+        if(num_children < len(all_children) - 1):
+            print(symbol + child_branch + str(children))
+        else:
+            end_flile = 1
+            print(symbol + child_branch_done + str(children))
 
+        if (os.path.isdir(os.path.join(path, children))):
+            dir_count = dir_count + 1
+            if(end_file == 1):
+                tree_generate(os.path.join(path, children), symbol + indent_done)
+            else:
+                tree_generate(os.path.join(path, children), symbol + indent)
+        elif(os.path.isfile(os.path.join(path, children))):
+                file_count = file_count + 1
+        num_children = num_children + 1
 
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+        # just for demo
+    # subprocess.run(['tree'] + sys.argv[1:])
+    if(len(sys.argv) == 2):
+        dir_path = sys.argv[1]
+    else:
+        dir_path = "."
+    print(dir_path)
+    tree_generate(dir_path, "")
+    print(str(dir_count) + " directories, " + str(file_count) + " files")
